861ac94d680f121caabed07671f7f69938a0a63d
MauricioAires <mauricioaires015@gmail.com>
2023-10-14 19:46:33 -0300

    chore(package) add @rocketseat/eslint-config and prettier-plugin-tailwindcss

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
-    <div className="p-8 bg-slate-50 dark:bg-slate-900 dark:text-slate-100 text-slate-900 h-screen flex flex-col items-center text-center">
-      <div className="max-w-2xl">
-        <h1 className="font-bold text-3xl sm:text-4xl">
+    <div className="flex h-screen flex-col  items-center bg-slate-900 p-8 text-center text-slate-100">
+      <div className="max-w-[700px]">
+        <h1 className="text-3xl font-bold sm:text-4xl">
           Rapidly build modern websites without ever leaving your HTML.
         </h1>
         <h2 className="mt-4 dark:bg-slate-400">
@@ -11,10 +11,10 @@ export default function Home() {
           directly in your markup. Get started Quick search... Ctrl K
         </h2>
 
-        <button className="bg-sky-500 dark:bg-sky-400 dark:text-sky-950 text-white px-4 py-2 rounded-md font-md mt-4 enabled:hover:bg-sky-600 disabled:cursor-not-allowed disabled:opacity-60 ">
+        <button className="font-md mt-4 rounded-md bg-sky-400 px-4 py-2 text-sky-950 enabled:hover:bg-sky-600 disabled:cursor-not-allowed disabled:opacity-60 dark:bg-slate-100 dark:font-serif  dark:text-slate-200 ">
           Get Started
         </button>
       </div>
     </div>
-  );
+  )
 }
