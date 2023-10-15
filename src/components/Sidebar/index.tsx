b57b3fea3de2bfca6befa87ffe967b5ec390a060
MauricioAires <mauricioaires015@gmail.com>
2023-10-14 21:02:23 -0300

    feat(app) create navigation menu

---
+++ src/components/Sidebar/index.tsx
@@ -0,0 +1,24 @@
+import { Search } from 'lucide-react'
+import { Logo } from './Logo'
+import { MainNavigation } from './MainNavigation'
+
+export function Sidebar() {
+  return (
+    <aside className="space-y-6 border-r border-zinc-200 px-5 py-8">
+      <Logo />
+
+      <div className="mx-1 flex w-full items-center gap-2 rounded-lg border border-zinc-300 px-2 py-2 shadow-sm">
+        <Search className="h-5 w-5 text-zinc-500" />
+        <input
+          className="flex-1 text-zinc-900 placeholder-zinc-600"
+          placeholder="Search"
+          type="search"
+          name="search"
+          id="search"
+        />
+      </div>
+
+      <MainNavigation />
+    </aside>
+  )
+}
