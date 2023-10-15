a8c01f758a36499b629a1186e1685b733f75e588
MauricioAires <mauricioaires015@gmail.com>
2023-10-14 21:24:34 -0300

    feat(sidebar) create used space widget

---
+++ src/components/Sidebar/UsedSpaceWidget.tsx
@@ -0,0 +1,18 @@
+export function UsedSpaceWidget() {
+  return (
+    <div className="space-x-3">
+      <button
+        type="button"
+        className="text-sm font-medium text-violet-500 hover:text-violet-700"
+      >
+        Dismiss
+      </button>
+      <button
+        type="button"
+        className="text-sm font-medium text-violet-700 hover:text-violet-900"
+      >
+        Upgrade plan
+      </button>
+    </div>
+  )
+}
--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,10 +1,21 @@
-import { Search } from 'lucide-react'
+import {
+  Search,
+  BarChart,
+  CheckSquare,
+  Flag,
+  Home,
+  SquareStack,
+  Users,
+  LifeBuoy,
+  Cog,
+} from 'lucide-react'
 import { Logo } from './Logo'
-import { MainNavigation } from './MainNavigation'
+import { NavItem } from './NavItem'
+import { UsedSpaceWidget } from './UsedSpaceWidget'
 
 export function Sidebar() {
   return (
-    <aside className="space-y-6 border-r border-zinc-200 px-5 py-8">
+    <aside className="flex flex-col gap-6 border-r border-zinc-200 px-5 py-8">
       <Logo />
 
       <div className="mx-1 flex w-full items-center gap-2 rounded-lg border border-zinc-300 px-2 py-2 shadow-sm">
@@ -18,7 +29,38 @@ export function Sidebar() {
         />
       </div>
 
-      <MainNavigation />
+      <nav className="space-y-0.5">
+        <NavItem title="Home" icon={Home} />
+        <NavItem title="Dashboard" icon={BarChart} />
+        <NavItem title="Projects" icon={SquareStack} />
+        <NavItem title="Tasks" icon={CheckSquare} />
+        <NavItem title="Reporting" icon={Flag} />
+        <NavItem title="Users" icon={Users} />
+      </nav>
+
+      <div className="mt-auto flex flex-col gap-6">
+        <nav className="space-y-0.5">
+          <NavItem title="Support" icon={LifeBuoy} />
+          <NavItem title="Settings" icon={Cog} />
+        </nav>
+
+        <div className="flex flex-col gap-4 rounded-lg bg-violet-50 px-4 py-5">
+          <div className="space-y-1">
+            <span className="text-sm/5 font-medium text-violet-700">
+              Used space
+            </span>
+            <p className="text-sm/5 text-violet-500">
+              Your team has used 80% of your available space. Need more?
+            </p>
+          </div>
+
+          <div className="h-2 rounded-full bg-violet-100">
+            <div className="h-2 w-4/5 rounded-full bg-violet-600"></div>
+          </div>
+
+          <UsedSpaceWidget />
+        </div>
+      </div>
     </aside>
   )
 }
