e3908faf9ea35352d2f9c3477cbcd038834a3e71
MauricioAires <mauricioaires015@gmail.com>
2023-10-14 22:32:13 -0300

    feat(sidebar) Composition Pattern

---
+++ src/components/Input.tsx
@@ -0,0 +1,48 @@
+import { ComponentProps } from 'react'
+
+/**
+ *
+ *  Composition Pattern
+ *
+ * É quando quebra um componente maior em componentes
+ * menores sendo possível agrupar esse componentes
+ * menores adiciona propriedades ao componente principal
+ */
+
+/**
+ * InputPrefix
+ */
+type InputPrefixProps = ComponentProps<'div'>
+
+export function Prefix(props: InputPrefixProps) {
+  return <div {...props} />
+}
+
+/**
+ * InputControl
+ */
+
+type InputControlProps = ComponentProps<'input'>
+
+export function Control(props: InputControlProps) {
+  return (
+    <input className="flex-1 text-zinc-900 placeholder-zinc-600" {...props} />
+  )
+}
+
+/**
+ * InputControl
+ */
+
+type InputRootProps = ComponentProps<'div'>
+
+export function Root(props: InputRootProps) {
+  return (
+    <div
+      className="mx-1 flex w-full items-center gap-2 rounded-lg border border-zinc-300 px-2 py-2 shadow-sm"
+      {...props}
+    >
+      {props.children}
+    </div>
+  )
+}
--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,5 +1,4 @@
 import {
-  Search,
   BarChart,
   CheckSquare,
   Flag,
@@ -8,27 +7,25 @@ import {
   Users,
   LifeBuoy,
   Cog,
+  Search,
 } from 'lucide-react'
 import { Logo } from './Logo'
 import { NavItem } from './NavItem'
 import { UsedSpaceWidget } from './UsedSpaceWidget'
 import { Profile } from './Profile'
+import * as Input from '../Input'
 
 export function Sidebar() {
   return (
     <aside className="flex flex-col gap-6 border-r border-zinc-200 px-5 py-8">
       <Logo />
 
-      <div className="mx-1 flex w-full items-center gap-2 rounded-lg border border-zinc-300 px-2 py-2 shadow-sm">
-        <Search className="h-5 w-5 text-zinc-500" />
-        <input
-          className="flex-1 text-zinc-900 placeholder-zinc-600"
-          placeholder="Search"
-          type="search"
-          name="search"
-          id="search"
-        />
-      </div>
+      <Input.Root>
+        <Input.Prefix>
+          <Search className="h-5 w-5 text-zinc-500" />
+        </Input.Prefix>
+        <Input.Control placeholder="Search" />
+      </Input.Root>
 
       <nav className="space-y-0.5">
         <NavItem title="Home" icon={Home} />
