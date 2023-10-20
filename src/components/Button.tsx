deef66f1e675544fabf05c87acbce8408aa7a663
MauricioAires <mauricioaires015@gmail.com>
2023-10-20 13:18:14 -0300

    feat(file input) API de Slots do Tailwind Variants

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -11,7 +11,7 @@ const button = tv({
     variant: {
       primary: 'bg-violet-600 text-white hover:bg-violet-700',
       outline: 'border border-zinc-300 text-zinc-700 bg-zinc-50',
-      ghost: 'rounded-md px-2 hover:bg-zinc-50 shadow-none',
+      ghost: 'rounded-md px-2 hover:bg-zinc-50 shadow-none text-zinc-500',
     },
   },
 
@@ -26,12 +26,13 @@ export type ButtonProps = ComponentProps<'button'> & VariantProps<typeof button>
  *  Não precisa especificar o children porque ele já vai ser
  * repassado pelo props
  */
-export function Button({ variant, ...props }: ButtonProps) {
+export function Button({ variant, className, ...props }: ButtonProps) {
   return (
     <button
       {...props}
       className={button({
         variant,
+        className,
       })}
     />
   )
